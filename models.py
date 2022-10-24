f0c9331e84bd717fda532a1bc215a7dd97ba7137
MarKamin <marius.kaminskas911206@gmail.com>
2022-10-24 15:35:11 +0300

    model update

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auto_moto_DB.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFIACTIONS'] = False
 
 db = SQLAlchemy(app)
@@ -31,21 +31,23 @@ class cars(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey('users.id'))
     car_user = db.relationship("users", backref=db.backref("moto_users", uselist=False))
-    brand = db.Column("Marke", db.String(66))
-    model = db.Column("Modelis", db.String(66))
+    brand = db.Column("marke", db.String(66))
+    model = db.Column("modelis", db.String(66))
     # year = db.Column("Metai", db.DateTime(datetime.date))
-    year = db.Column("Metai", db.String(4))
-    kuras = db.Column("Kuro_tipas", db.String(99))
-    kebulas = db.Column("Kebulas", db.String(99))
-    kaina = db.Column("Kaina(€)", db.Float())
-    vin_code = db.Column("VIN_kodas", db.String(99))
-    date_created = db.Column('Sukurimo data', db.DateTime(timezone=True), server_default=func.now())
+    year = db.Column("metai", db.String(4))
+    rida = db.Column("rida", db.Float())
+    kuras = db.Column("kuro_tipas", db.String(99))
+    kebulas = db.Column("kebulas", db.String(99))
+    kaina = db.Column("kaina", db.Float())
+    vin_code = db.Column("vin_kodas", db.String(99))
+    date_created = db.Column('sukurimo data', db.DateTime(timezone=True), server_default=func.now())
 
-    def __init__(self, user_id, brand, model, year, kuras, kebulas, kaina, vin_code):
+    def __init__(self, user_id, brand, model, year, rida, kuras, kebulas, kaina, vin_code):
         self.user_id = user_id
         self.brand = brand
         self.model = model
         self.year = year
+        self.rida = rida
         self.kuras = kuras
         self.kebulas = kebulas
         self.kaina = kaina
@@ -56,22 +58,24 @@ class moto(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey('users.id'))
     moto_user = db.relationship("users", backref=db.backref("users", uselist=False))
-    brand = db.Column("Marke", db.String(66))
-    model = db.Column("Modelis", db.String(66))
-    tipas = db.Column('Tipas', db.String(66))
-    year = db.Column("Metai", db.String(4))
-    cc = db.Column("Galingumas", db.Integer())
-    kaina = db.Column("Kaina(€)", db.Float())
-    svoris = db.Column("Svoris", db.Float())
-    date_created = db.Column('Sukurimo data', db.DateTime(timezone=True), server_default=func.now())
+    brand = db.Column("marke", db.String(66))
+    model = db.Column("modelis", db.String(66))
+    tipas = db.Column('tipas', db.String(66))
+    year = db.Column("metai", db.String(4))
+    rida = db.Column('rirda', db.Float())
+    cc = db.Column("galingumas", db.Integer())
+    kaina = db.Column("Kkaina", db.Float())
+    svoris = db.Column("svoris", db.Float())
+    date_created = db.Column('sukurimo data', db.DateTime(timezone=True), server_default=func.now())
     
 
-    def __init__(self, user_id, brand, model, tipas, year, cc, kaina, svoris):
+    def __init__(self, user_id, brand, model, tipas, year, rida, cc, kaina, svoris):
         self.user_id = user_id
         self.brand = brand
         self.model = model
         self.tipas = tipas
         self.year = year
+        self.rida = rida
         self.cc = cc
         self.kaina = kaina
         self.svoris = svoris
